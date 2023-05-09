b589da964937a2756901897d295b5ec5eec62601
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-05-09 15:30:45 -0300

    1.1

--- Arquivos_Projeto/backend/app_41.js
+++ Arquivos_Projeto/backend/app_41.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const sqlite3 = require('sqlite3').verbose();
-const DBPATH = '../data/BancoUser.db';
+const DBPATH = '../data/Banco_Projeto.db';
 
 const hostname = '127.0.0.1';
-const port = 2020;
+const port = 2040;
 const app = express();
 
 /* Colocar toda a parte estática no frontend */
@@ -16,12 +16,12 @@ app.use(express.static("../frontend/"));
 /******** CRUD ************/
 app.use(express.json());
 
-// Retorna todos registros (é o R do CRUD - Read)
+// Retorna todos registros da tabela USUARIO (é o R do CRUD - Read)
 app.get('/usuarios', (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
-	var sql = 'SELECT * FROM TblFORMACAO ORDER BY CURSO COLLATE NOCASE';
+	var sql = 'SELECT * FROM USUARIO ORDER BY Telefone COLLATE NOCASE';
 		db.all(sql, [],  (err, rows ) => {
 			if (err) {
 				throw err;
@@ -31,28 +31,28 @@ app.get('/usuarios', (req, res) => {
 		db.close(); // Fecha o banco
 });
 
-// Insere um registro (é o C do CRUD - Create)
+// Insere um registro na tabela USUARIO (é o C do CRUD - Create)
 app.post('/insereUsuario', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
-	sql = "INSERT INTO TblFORMACAO (CURSO, DATA_INI, DATA_FIM, DESCRICAO) VALUES ('" + req.body.CURSO + "', '" + req.body.DATA_INI + "', '" + req.body.DATA_FIM + "', '" + req.body.DESCRICAO + "')";
+	sql = "INSERT INTO USUARIO (Telefone, Email, Senha, Categoria) VALUES ('" + req.body.Telefone + "', '" + req.body.Email + "', '" + req.body.Senha + "', '" + req.body.Categoria + "')";
 	console.log(sql);
 	db.run(sql, [],  err => {
 		if (err) {
 		    throw err;
 		}	
 	});
-	res.write('<p>FORMACAO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	res.write('<p>USUARIO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
 	db.close(); // Fecha o banco
 	res.end();
 });
 
-// Monta o formulário para o update (é o U do CRUD - Update)
+// Monta o formulário para o update da tabela USUARIO (é o U do CRUD - Update)
 app.get('/atualizaUsuario', (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
-	sql = "SELECT * FROM TblFORMACAO WHERE ID_FORMACAO="+ req.query.ID_FORMACAO;
+	sql = "SELECT * FROM USUARIO WHERE Id_Usuario="+ req.query.Id_usuario;
 	console.log(sql);
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.all(sql, [],  (err, rows ) => {
@@ -64,11 +64,11 @@ app.get('/atualizaUsuario', (req, res) => {
 	db.close(); // Fecha o banco
 });
 
-// Atualiza um registro (é o U do CRUD - Update)
+// Atualiza um registro da tabela USUARIO (é o U do CRUD - Update)
 app.post('/atualizaUsuario', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
-	sql = "UPDATE TblFORMACAO SET CURSO='" + req.body.CURSO + "', DATA_INI = '" + req.body.DATA_INI + "' , DATA_FIM='" + req.body.DATA_FIM + "' , DESCRICAO='" + req.body.DESCRICAO + "' WHERE ID_FORMACAO='" + req.body.ID_FORMACAO + "'";
+	sql = "UPDATE USUARIO SET Telefone='" + req.body.Telefone + "', Email= '" + req.body.Email + "' , Senha='" + req.body.Senha + "' , Categoria='" + req.body.Categoria + "' WHERE Id_Usuario= " + req.body.Id_Usuario;
 	console.log(sql);
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.run(sql, [],  err => {
@@ -77,27 +77,196 @@ app.post('/atualizaUsuario', urlencodedParser, (req, res) => {
 		}
 		res.end();
 	});
-	res.write('<p>FORMACAO ATUALIZADA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	res.write('<p>USUARIO ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
 	db.close(); // Fecha o banco
 });
 
-// Exclui um registro (é o D do CRUD - Delete)
+// Exclui um registro da tabela USUARIO (é o D do CRUD - Delete)
 app.get('/removeUsuario', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
-	sql = "DELETE FROM TblFORMACAO WHERE ID_FORMACAO='" + req.query.ID_FORMACAO + "'";
+	sql = "DELETE FROM USUARIO WHERE Id_Usuario=" + req.body.Id_Usuario;
 	console.log(sql);
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.run(sql, [],  err => {
 		if (err) {
 		    throw err;
 		}
-		res.write('<p>FORMACAO REMOVIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+		res.write('<p>USUARIO REMOVIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
 		res.end();
 	});
 	db.close(); // Fecha o banco
+});
+
+// Retorna todos os registros da tabela PERGUNTA (é o R do CRUD - Reading)
+app.get('/pergunta', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM PERGUNTA ORDER BY Pergunta COLLATE NOCASE';
+		db.all(sql, [],  (err, rows ) => {
+			if (err) {
+				throw err;
+			}
+			res.json(rows);
+		});
+		db.close(); // Fecha o banco
+});
+
+// Insere uma pergunta na tabela PERGUNTA (é o C do CRUD - Create)
+app.post('/inserePergunta', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	sql = "INSERT INTO PERGUNTA (Pergunta) VALUES ('" + req.body.Pergunta + "')";
+	console.log(sql);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}	
+	});
+	res.write('<p>PERGUNTA INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+	res.end();
 });
 
 app.listen(port, hostname, () => {
   console.log(`Servidor rodando em http://${hostname}:${port}/`);
 });
+
+// Monta o formulário para o update da tabela PERGUNTA (é o U do CRUD - Update)
+app.get('/atualizaPergunta', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "SELECT * FROM PERGUNTA WHERE Id_Pergunta="+ req.query.Id_Pergunta;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Atualiza um registro da tabela PERGUNTA (é o U do CRUD - Update)
+app.post('/atualizaPergunta', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "UPDATE PERGUNTA SET Pergunta='" + req.body.Pergunta + "' WHERE Id_Pergunta= " + req.body.Id_Pergunta;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	res.write('<p>PERGUNTA ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro da tabela PERGUNTA (é o D do CRUD - Delete)
+app.get('/removePergunta', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "DELETE FROM PERGUNTA WHERE Id_Pergunta=" + req.body.Id_Pergunta;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.write('<p>PERGUNTA REMOVIDA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+// Insere a atividade do protocolo na tabela PROTOCOLO (é o C do CRUD - Create)
+app.post('/insereAtividadeDoProtocolo', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	sql = "INSERT INTO PROTOCOLO (Atividade) VALUES ('" + req.body.Atividade + "')";
+	console.log(sql);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}	
+	});
+	res.write('<p>ATIVIDADE DO PROTOCOLO INSERIDA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Retorna todos registros da tabela PROTOCOLO (é o R do CRUD - Read)
+app.get('/protocolo', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM PROTOCOLO ORDER BY Atividade COLLATE NOCASE';
+		db.all(sql, [],  (err, rows ) => {
+			if (err) {
+				throw err;
+			}
+			res.json(rows);
+		});
+		db.close(); // Fecha o banco
+});
+
+// Monta o formulário para o update da tabela PROTOCOLO (é o U do CRUD - Update)
+app.get('/atualizaProtocolo', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "SELECT * FROM PROTOCOLO WHERE Id_Protocolo="+ req.query.Id_Protocolo;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Atualiza um registro da tabela PROTOCOLO (é o U do CRUD - Update)
+app.post('/atualizaProtocolo', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "UPDATE PROTOCOLO SET Atividade='" + req.body.Atividade + "' WHERE Id_Protocolo= " + req.body.Id_Protocolo;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	res.write('<p>PROTOCOLO ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro da tabela PROTOCOLO (é o D do CRUD - Delete)
+app.get('/removeProtocolo', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "DELETE FROM PROTOCOLO WHERE Id_Protocolo=" + req.body.Id_Protocolo;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.write('<p>ATIVIDADE DO PROTOCOLO REMOVIDA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+
+
+
+
