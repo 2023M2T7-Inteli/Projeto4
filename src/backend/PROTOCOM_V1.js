4187d530751688bdeba0b8385d9679b771c8fd91
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-06-03 21:19:52 -0300

    Integração da página de sing_up

--- src/backend/PROTOCOM_V1.js
+++ src/backend/PROTOCOM_V1.js
@@ -21,10 +21,10 @@ app.post('/insereUsuario', urlencodedParser, (req, res) => {
     res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
     var db = new sqlite3.Database(DBPATH); // Abre o banco
-    const { Telefone, Email, Senha, Categoria, Nome } = req.body;
-    sql = "INSERT INTO USUARIO (Telefone, Email, Senha, Categoria, Nome) VALUES (?, ?, ?, ?, ?)";
+    const { Telefone, Email, Senha, Categoria, Nome, TipoDePlantacao } = req.body;
+    sql = "INSERT INTO USUARIO (Telefone, Email, Senha, Categoria, Nome, TipoDePlantacao) VALUES (?, ?, ?, ?, ?, ?)";
     console.log(sql);
-    db.run(sql, [Telefone, Email, Senha, Categoria, Nome], err => {
+    db.run(sql, [Telefone, Email, Senha, Categoria, Nome, TipoDePlantacao], err => {
         if (err) {
             throw err;
         }
--- src/frontend/sign_up/sign_up.js
+++ src/frontend/sign_up/sign_up.js
@@ -1,17 +1,80 @@
-/*Este código usa jQuery para manipular eventos de clique
-nos botões de rádio. Com base no botão selecionado, os elementos com a classe "option1"
-são mostrados se for a opção 1 selecionada, caso contrário, são ocultados. Os elementos com a classe "option2"
-são mostrados se não for a opção 1 selecionada, caso contrário, são ocultados.*/
+var nome = "";
+var email = "";
+var senha = "";
+var tipoDePlantacao = "";
+var telefone = "";
 
-$(document).ready(function() {
-  $('input[type="radio"]').click(function() {
-    var selectedValue = $(this).attr('id');
-    if (selectedValue === "option1") {
-      $('.option1').show();
-      $('.option2').hide();
-    } else {
-      $('.option1').hide();
-      $('.option2').show();
+function inserirResposta(nome, email, senha, tipoDePlantacao, telefone, categoria) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:2021/insereUsuario', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      // A requisição foi concluída e a resposta está pronta
+      if (xhr.status === 200) {
+        // A requisição foi bem-sucedida (status 200)
+        console.log('Requisição bem-sucedida');
+        // Aqui você pode realizar qualquer lógica adicional com a resposta recebida do servidor
+      } else {
+        // A requisição falhou com um status diferente de 200
+        console.log('Erro na requisição: ' + xhr.status);
+      }
     }
-  });
-});
+  };
+
+  if (nome === "" || email === "" || senha === "" || tipoDePlantacao === "" || telefone === "") {
+    console.log("não foi possível cadastrar o usuário");
+    exibirToast03();
+  } else {
+    var params = new URLSearchParams();
+    params.append('Nome', nome);
+    params.append('Email', email);
+    params.append('Senha', senha);
+    params.append('TipoDePlantacao', tipoDePlantacao);
+    params.append('Telefone', telefone);
+    params.append('Categoria', categoria);
+
+    console.log("cadastro feito com sucesso");
+
+    setTimeout(() => {xhr.send(params.toString())}, 3000)
+
+    exibirToast();
+  }
+}
+
+function criarConta() {
+  var nome = document.getElementById('idNome').value;
+  var email = document.getElementById('idEmail').value;
+  var senha = document.getElementById('idSenha').value;
+  var tipoDePlantacao = document.getElementById('idPla').value;
+  var telefone = document.getElementById('idTel').value;
+  var categoria = "Agricultor";
+
+  inserirResposta(nome, email, senha, tipoDePlantacao, telefone, categoria);
+}
+
+function exibirToast () {
+  exibirToast02();
+}
+
+function exibirToast02() {
+  Toastify({
+    text: "CADASTRO FEITO COM SUCESSO!",
+    duration: 3000, // Duração em milissegundos
+    close: true, // Mostrar botão de fechar
+    gravity: "top", // Posição do toast (top, bottom, left, right)
+    position: "right", // Alinhamento do toast (left, center, right)
+  }).showToast();
+}
+
+function exibirToast03() {
+  Toastify({
+    text: "CADASTRO NÃO REALIZADO, VERIFIQUE SE FORAM PREENCHIDOS TODOS OS CAMPOS!",
+    duration: 3000, // Duração em milissegundos
+    close: true, // Mostrar botão de fechar
+    gravity: "top", // Posição do toast (top, bottom, left, right)
+    position: "right", // Alinhamento do toast (left, center, right)
+  }).showToast();
+}
+
