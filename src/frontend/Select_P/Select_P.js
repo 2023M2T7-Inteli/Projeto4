00c6f97e5951b9339df20039ff98772f389aee2b
RicardoBMN <ricardo.novaes@sou.inteli.edu.br>
2023-06-07 09:55:06 -0300

    Select_P
    atualizado

--- src/frontend/Select_P/Select_P.js
+++ src/frontend/Select_P/Select_P.js
@@ -26,3 +26,10 @@ document.querySelectorAll('.buttons .indicator').forEach(button => {
         img.src = originalSrc; // restore the original image
     });
 });
+
+var urlParams = new URLSearchParams(window.location.search);
+var userId = urlParams.get('id');
+
+function voltarTela(){
+    window.location.href = "/agricultor/agricultor.html?id=" + userId;
+}
