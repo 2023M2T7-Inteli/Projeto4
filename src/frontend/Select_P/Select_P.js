afc54dee9367945fedbe950ce1b6fc063a21ff0b
DiogoBurgierman <124000765+DiogoBurgierman@users.noreply.github.com>
2023-06-12 15:13:28 -0300

    Atualização na tela Select_P
    Tela de seleção de protocolos por parte do extrativista atualizada, agora os protocolos aparecem na interface de forma dinâmica

--- src/frontend/Select_P/Select_P.js
+++ src/frontend/Select_P/Select_P.js
@@ -1,3 +1,89 @@
+fetch('http://localhost:2021/protocolo')
+.then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request error');
+  })
+  .then(function(data) {
+    
+    for(i=0;i<data.length;i++){
+     
+      let nomeProtocolo = data[i].Nome_Protocolo;
+      let descProtocolo = data[i].Descricao;
+      let dataProtocolo = data[i].Data;
+        
+        // Cria o elemento div com a classe "button_div"
+        const buttonDiv = document.createElement("div");
+        buttonDiv.className = "button_div";
+      
+        // Cria o elemento div com a classe "circle"
+        const circleDiv = document.createElement("div");
+        circleDiv.className = "circle";
+      
+        // Cria o elemento button com a classe "indicator"
+        const buttonElement = document.createElement("button");
+        buttonElement.className = "indicator";
+      
+        // Cria o elemento div com a classe "button-info"
+        const buttonInfoDiv = document.createElement("div");
+        buttonInfoDiv.className = "button-info";
+      
+        // Cria o elemento div com a classe "text-container"
+        const textContainerDiv = document.createElement("div");
+        textContainerDiv.className = "text-container";
+      
+        // Cria o elemento h2 para o título
+        const h2Element = document.createElement("h2");
+        h2Element.textContent = nomeProtocolo;
+      
+        // Cria o elemento p para o texto
+        const pElement = document.createElement("p");
+        pElement.textContent = descProtocolo;
+      
+        // Cria o elemento div com a classe "date-container"
+        const dateContainerDiv = document.createElement("div");
+        dateContainerDiv.className = "date-container";
+      
+        // Cria a imagem
+        const imgElement = document.createElement("img");
+        imgElement.src = "images/time_copy.png";
+        imgElement.alt = "Imagem do botão 1";
+      
+        // Cria o elemento p para a data
+        const pDateElement = document.createElement("p");
+        pDateElement.textContent = dataProtocolo;
+      
+        // Adiciona os elementos criados à estrutura HTML
+        textContainerDiv.appendChild(h2Element);
+        textContainerDiv.appendChild(pElement);
+      
+        dateContainerDiv.appendChild(imgElement);
+        dateContainerDiv.appendChild(pDateElement);
+      
+        buttonInfoDiv.appendChild(textContainerDiv);
+        buttonInfoDiv.appendChild(dateContainerDiv);
+      
+        buttonElement.appendChild(buttonInfoDiv);
+      
+        buttonDiv.appendChild(circleDiv);
+        buttonDiv.appendChild(buttonElement);
+      
+        // Encontra o elemento pai onde o trecho será adicionado
+        const container = document.getElementById("buttons");
+      
+        // Adiciona o trecho ao elemento pai
+        container.appendChild(buttonDiv);
+
+      }
+
+    })
+        
+      
+      
+
+      
+
 // Select all elements with the class 'button_div'
 document.querySelectorAll('.button_div').forEach(buttonDiv => {
     // Add event listener for click event on the 'indicator' child of each 'button_div'
