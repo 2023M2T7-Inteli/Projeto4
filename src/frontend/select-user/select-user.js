3da80aa09a666044ab064604d1de66094fb28f6b
DiogoBurgierman <124000765+DiogoBurgierman@users.noreply.github.com>
2023-06-22 19:51:27 -0300

    Correção de erro que levava para um endereço de imagem que nao existia deixando uma imagem faltando na tela

--- src/frontend/select-user/select-user.js
+++ src/frontend/select-user/select-user.js
@@ -6,19 +6,6 @@ const params = new URLSearchParams(window.location.search);
 
 window.onload = function() {
     endpoint();
-    let buttons = document.querySelectorAll('.user-button');
-    
-    buttons.forEach(button => {
-        button.addEventListener('mouseenter', function() {
-            let img = this.querySelector('.profile-picture');
-            img.setAttribute('src', 'images/profile3.png');
-        });
-
-        button.addEventListener('mouseleave', function() {
-            let img = this.querySelector('.profile-picture');
-            img.setAttribute('src', 'images/profile.png');
-        });
-    });
 };
 
 function sendToCreateProtocols() {
